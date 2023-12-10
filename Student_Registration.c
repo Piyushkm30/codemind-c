7bd3c78d08f7af806380769aec274a0ffdd15605
Piyushkm30 <147373038+Piyushkm30@users.noreply.github.com>
2023-12-10 16:30:55 +0530

    #include<stdio.h> int main () {     int T;     scanf("%d",&T);     int N,M,K;     scanf("%d%d%d",&N,&M,&K);     if(M-K>=N)     {         printf("YES ");     }     else {         printf("NO ");     }     scanf("%d%d%d",&N,&M,&K);     if(M-K>=N)     {         printf("YES ");     }     else{         printf("NO ");     } }

---
+++ Student_Registration.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main ()
+{
+    int T;
+    scanf("%d",&T);
+    int N,M,K;
+    scanf("%d%d%d",&N,&M,&K);
+    if(M-K>=N)
+    {
+        printf("YES
+");
+    }
+    else {
+        printf("NO
+");
+    }
+    scanf("%d%d%d",&N,&M,&K);
+    if(M-K>=N)
+    {
+        printf("YES
+");
+    }
+    else{
+        printf("NO
+");
+    }
+}
