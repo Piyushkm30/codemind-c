f38a0278a2e933e63599dd10deb5c0c997894a98
Piyushkm30 <147373038+Piyushkm30@users.noreply.github.com>
2023-12-18 14:30:10 +0530

    #include<stdio.h> int main () {     int a;     scanf("%d",&a);     int b=a,rev=0;     for(;a>0;a=a/10){         rev=(rev*10)+a%10;     }     if(rev==b)printf("Palindrome");     else printf("Not Palindrome"); }

---
+++ Palindrome.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{
+    int a;
+    scanf("%d",&a);
+    int b=a,rev=0;
+    for(;a>0;a=a/10){
+        rev=(rev*10)+a%10;
+    }
+    if(rev==b)printf("Palindrome");
+    else printf("Not Palindrome");
+}
