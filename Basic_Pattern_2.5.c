420daeae1dcb27410d35546d777b1e230b07e764
Piyushkm30 <147373038+Piyushkm30@users.noreply.github.com>
2024-01-03 13:44:05 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);    int x=64+a;     for(int i=1;i<=a;i++,x--){         for(int j=1;j<=i;j++){             printf("%c ",x);         }         printf(" ");     } }

---
+++ Basic_Pattern_2.5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+   int x=64+a;
+    for(int i=1;i<=a;i++,x--){
+        for(int j=1;j<=i;j++){
+            printf("%c ",x);
+        }
+        printf("
+");
+    }
+}
